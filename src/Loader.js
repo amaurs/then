84b4f4d0f13a835ec2077cf8dffd81faabdfca22
amaurs <amaury.gtz@gmail.com>
2019-09-05 09:16:29 -0700

    ⏰ Added loader to wait for the response to come back from the server.

---
+++ src/Loader.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Loader.css'
+
+export default class Loader extends Component {
+
+    render() {
+        return <h1 className="Loader">Loading...</h1>
+    }
+
+}
