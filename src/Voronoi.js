90d353c81963664b1a2a27084ea13cdd9dcf79f5
amaurs <amaury.gtz@gmail.com>
2019-09-10 08:37:39 -0700

    🖍 Using canvas instead of svg as renderer.

--- src/Voronoi.js
+++ src/Voronoi.js
@@ -14,11 +14,8 @@ const colorMap = {
     "black": BLACK,
 }
 
-const colorArray = [BLUE, YELLOW, RED, BLACK];
-
-
 function getRadius(d) {
-    return  2 + 1.5 * getBrightness(d.r, d.g, d.b);
+    return  3 + 3 * getBrightness(d.r, d.g, d.b);
 }
 
 
@@ -26,24 +23,27 @@ export default class Voronoi extends Component {
 
   constructor(props) {
     super(props);
+    this.canvasRef = React.createRef();
     this.state = {
       time: 0,
       sites: null,
       steps: 5,
-      colors: ["red", "yellow", "blue", "black"]
     };
   }
 
   componentDidMount() {
     let imageData = this.props.imageData;
     let imageWidth = this.props.imageWidth;
+    let imageHeight = this.props.imageHeight;
+    let width = this.props.width;
+    let height = this.props.height;
     let sites = [];
-    let total = Math.floor((imageWidth / xStep) * (this.props.imageHeight / yStep));
+    let total = Math.floor((imageWidth / xStep) * (imageHeight / yStep));
 
     /** I use the rejection algorithm to get points with the most brightness. **/
     let numPoints = 0;
     while(numPoints < total){
-      let index = getRandomInt(0, imageWidth * this.props.imageHeight);
+      let index = getRandomInt(0, imageWidth * imageHeight);
       let site = imageData[index]
       let brightness = getBrightness(site.r, 
                                      site.g, 
@@ -53,12 +53,24 @@ export default class Voronoi extends Component {
         numPoints++;
       }
     }
-    let sitesNew = this.sitesUpdate(sites, imageData, imageWidth, this.props.imageHeight);
+    let sitesNew = this.sitesUpdate(sites, imageData, imageWidth, imageHeight);
+
+    let i = 0;
+    while(i < 15){
+        sitesNew = this.sitesUpdate(sitesNew, imageData, imageWidth, imageHeight);
+        i++;
+    }
+
+
 
     this.setState({sites:sitesNew});
 
+
+    
+
+
     this.cities = d3.select(this.svg)
-                    .attr("viewBox", "0 0 " + imageWidth + " " + this.props.imageHeight)
+                    .attr("viewBox", "0 0 " + imageWidth + " " + imageHeight)
                     .append("g")
                     .attr("class", "sites")
                     .selectAll("circle")
@@ -68,26 +80,41 @@ export default class Voronoi extends Component {
                     .attr("cx", function(d) { return +d.x })
                     .attr("cy", function(d) { return +d.y })
                     .attr("r", getRadius)
-                    .style("fill", function(d) { return  d3.rgb(d.r, d.g, d.b) })
+                    .style("fill", function(d) { return  d3.rgb(+d.r, +d.g, +d.b) })
                     .style("stroke-width", "0");
 
-    this.timerID = setInterval(() => this.tick(), 100);
-  }
+    let custom = d3.select(document.createElement('custom'))
 
-  onButtonClick(type) {
-    console.log(type);
-    let colorsCopy = this.state.colors.slice();
-    if(colorsCopy.indexOf(type) < 0) {
-        colorsCopy.push(type);
-    } else {
-        colorsCopy.splice( colorsCopy.indexOf(type), 1 );
-    }
-    let final = this.risogrify(this.state.sites.slice(), colorsCopy);
-    this.setState({colors: colorsCopy, final: final});
+    let selection = custom
+                    .selectAll('custom.circle')
+                    .data(sitesNew)
+                    .enter()
+                    .append("custom.circle")
+                    .attr('x', function(d) { return (+d.x / imageWidth) * width })
+                    .attr('y', function(d) { return (+d.y / imageHeight) * height })
+                    .attr('r', getRadius)
+                    .attr('fillStyle', function(d) { return  d3.rgb(+d.r, +d.g, +d.b) });
+
+    let context = this.canvasRef.current.getContext('2d');
+    context.save();
+    context.clearRect(0, 0, this.width, this.height);
+    
+    selection.each(function(d, i) {
+        let node = d3.select(this);
+        context.beginPath();
+        context.fillStyle = node.attr('fillStyle');
+        context.arc(node.attr('x'), node.attr('y'), node.attr('r'), 0, 2 * Math.PI);
+        context.fill();
+    });
+
+
+
+
+
+    //this.timerID = setInterval(() => this.tick(), 100);
   }
 
   componentWillUnmount() {
-    console.log("Voronoi will unmount.");
     clearInterval(this.timerID);
     this.cities = d3.select(this.svg)
                     .append("g")
@@ -96,21 +123,6 @@ export default class Voronoi extends Component {
                     .data([])
                     .remove();
     
-  }
-
-  handleExport() {
-    
-    let final = this.state.final.slice();
-
-    let name = this.state.colors.reduce(function(a, b) { return a + "-" + b}, "");
-
-
-    this.print(final.filter(function(site) { return !(site.r === 255 && site.g === 255 && site.b === 255) }), "all");
-
-    //this.print(final.filter(function(site) { return site.r === 255 && site.g === 0 && site.b === 0}), "red")
-    //this.print(final.filter(function(site) { return site.r === 255 && site.g === 255 && site.b === 0}), "yellow")
-    //this.print(final.filter(function(site) { return site.r === 0 && site.g === 0 && site.b === 255}), "blue")
-    //this.print(final.filter(function(site) { return site.r === 0 && site.g === 0 && site.b === 0}), "black")
   }
 
   sitesUpdate(sites, imageData, width, height) {
@@ -140,23 +152,31 @@ export default class Voronoi extends Component {
 
   tick(){
     let imageWidth = this.props.imageWidth;
+    let imageHeight = this.props.imageHeight;
     let imageData = this.props.imageData;
     let time = this.state.time;
-    let sitesNew = this.sitesUpdate(this.state.sites, imageData, imageWidth, this.props.imageHeight)
+    let sitesNew = this.sitesUpdate(this.state.sites, imageData, imageWidth, imageHeight);
 
     
     this.setState({sites:sitesNew, time: time + 1});
-
+    
     this.update(sitesNew);  
+
 
     if(this.state.time > 15) {
       clearInterval(this.timerID);
-      let final = this.risogrify(this.state.sites.slice(), this.state.colors);
-      this.setState({sites:sitesNew, final: final});
+      this.setState({sites:sitesNew});
     }
+
   }
 
-  update(sites, alpha=false) {
+  draw(){
+
+  }
+
+  update(sites) {
+    
+
     d3.select(this.svg)
       .selectAll('circle')
       .data(sites)
@@ -166,125 +186,15 @@ export default class Voronoi extends Component {
       .attr("cy", function(d) { return +d.y })
       .attr("r", function(d) { return d.radius? +d.radius : getRadius(d) })
       .style("fill", function(d) {
-        let color = d3.rgb(d.r, d.g, d.b)
-        if(alpha) {
-            color.opacity = d.a;
-        }
+        let color = d3.rgb(d.r, d.g, d.b);
         return color; })
-      .style("stroke", function(d) {
-        let color = d3.rgb(d.r, d.g, d.b)
-        if(alpha) {
-            color.opacity = d.a;
-        }
-        return d3.rgb(255, 255, 255); })
       .style("stroke-width", "0");
   }
 
-  print(printSites, suffix) {
-    let _svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-
-    let alpha = true;
-
-    let size = printSites.length;
-
-    d3.select(_svg)
-        .attr("width", this.props.imageWidth)
-        .attr("height", this.props.imageHeight);
-
-
-    d3.select(_svg)
-      .append("g")
-      .attr("class", "sites")
-      .selectAll("circle")
-      .data(printSites)
-      .enter()
-      .append("circle")
-      .attr("cx", function(d) { return +d.x })
-      .attr("cy", function(d) { return +d.y })
-      .attr("r", function(d) { return +d.radius })
-      .style("fill", function(d) {
-        let color = d3.rgb(d.r, d.g, d.b);
-        return color; })
-      .style("stroke-width", "0")
-      .style("opacity", function(d){ return d.a });
-
-    console.log("about to print")
-
-    let serializer = new XMLSerializer();
-    let name = "our-lady-" + suffix + "-" + size;
-
-    let ohter = this.svg;
-
-    let source = serializer.serializeToString(_svg);
-    download(name + ".svg", source);
-  }
-
-  risogrify(coloredSites, colors) {
-      console.log("final pass to get colors");
-      //let coloredSites = this.state.sites.slice();
-
-      let tree = d3.quadtree()
-        .x(function(d) { return d.x })
-        .y(function(d) { return d.y }).addAll(coloredSites);
-
-      
-      let final = [];
-      console.log(colors)
-      let colorsNow = colors.map(function(color) {
-        return colorMap[color];
-      });
-
-
-
-      coloredSites.forEach(function(site) {
-        let treeCopy = tree.copy();
-        let oldColor = [site.r, site.g, site.b]
-        let newColor = closest(colorsNow, site.r, site.g, site.b);
-        let error = [site.r - newColor.flat_r,
-                     site.g - newColor.flat_g,
-                     site.b - newColor.flat_b]
-        final.push({
-            x: site.x,
-            y: site.y,
-            r: newColor.alpha_r,
-            g: newColor.alpha_g,
-            b: newColor.alpha_b,
-            a: newColor.alpha_a,
-            radius: getRadius(site)
-        });
-        let current = site;
-        [ 7.0 / 16, 5.0 / 16, 3.0 / 16, 1.0 / 16 ].forEach(function(d) {
-            if(typeof current !== "undefined") {
-                treeCopy.remove(current);
-                let found = treeCopy.find(site[0], site[1]);
-                if(typeof found !== "undefined") {
-                    found.r = Math.round(found.r + error[0] * d)
-                    found.g = Math.round(found.g + error[1] * d)
-                    found.b = Math.round(found.b + error[2] * d)
-                } 
-            }
-        });
-        tree.remove(site);
-      });
-    console.log(tree.size());
-    this.update(final, true);
-
-    return final;
-  }
-
-
-
   render() {
-    console.log(this.state.colors);
-    return <div>
-            <svg ref={ svg => this.svg = svg } />
-            <div className="controls">
-                <button className={(this.state.colors.indexOf("red") < 0 ? "" : "red")} onClick={() => this.onButtonClick("red")}>Red</button>
-                <button className={(this.state.colors.indexOf("yellow") < 0 ? "" : "yellow")} onClick={() => this.onButtonClick("yellow")}>Yellow</button>
-                <button className={(this.state.colors.indexOf("blue") < 0 ? "" : "blue")} onClick={() => this.onButtonClick("blue")}>Blue</button>
-                <button className={(this.state.colors.indexOf("black") < 0 ? "" : "black")} onClick={() => this.onButtonClick("black")}>Black</button>
-                <button onClick={() => this.handleExport()}>Export all</button>
+    return  <div>
+             <canvas width={this.props.width + "px"} height={this.props.height + "px"} className="VoronoiCanvas" ref={this.canvasRef}></canvas>
+             <svg ref={ svg => this.svg = svg } />;
             </div>
-           </div>;
   }
 }
