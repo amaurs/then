56c2c8d9fdae32d475147c8a1da39912888abc57
Amaury Gutierrez <acoamaur@amazon.com>
2019-11-24 13:29:01 -0800

    😋 Deploying the new section.

--- src/Circle.js
+++ src/Circle.js
@@ -3,126 +3,76 @@ import useInterval from './Hooks.js';
 
 import Board from './Board.js';
 
-const getPixelRatio = context => {
-    let backingStore =
-    context.backingStorePixelRatio ||
-    context.webkitBackingStorePixelRatio ||
-    context.mozBackingStorePixelRatio ||
-    context.msBackingStorePixelRatio ||
-    context.oBackingStorePixelRatio ||
-    context.backingStorePixelRatio ||
-    1;
-    
-    return (window.devicePixelRatio || 1) / backingStore;
-};
-
-
 let board = new Board(100, 100);
 
-board.randomize();
+board.init();
 
+board.gliderGun(Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), );
+board.gliderGun(Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), );
+board.gliderGun(Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), );
+board.gliderGun(Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), );
 
 
 const Circle = () => {
     let ref = useRef();
-
+    const squareSize = 10;
     const [count, setCount] = useState(0);
     const [drag, setDrag] = useState(false);
-    const [position, setPosition] = useState([0,0]);
+    const [position, setPosition] = useState([48 * squareSize,48 * squareSize]);
     const [offset, setOffset] = useState([0,0]);
-    const squareSize = 10;
     const square = [8 * squareSize, 3 * squareSize];
 
-
-
-
     useInterval(() => {
-        
         board = board.getNextGeneration();
         let canvas = ref.current;
         let context = canvas.getContext('2d');
-        let ratio = getPixelRatio(context);
-        
-        /**
-        let width = getComputedStyle(canvas)
-            .getPropertyValue('width')
-            .slice(0, -2);
-        let height = getComputedStyle(canvas)
-            .getPropertyValue('height')
-            .slice(0, -2);
-        **/
-
         
         context.clearRect(0, 0, canvas.width, canvas.height);
-
         context.fillStyle = "black";
-
         let color = board.getColor(context, squareSize, position[0] / squareSize, 
                                              position[1] / squareSize, 
                                              square[0] / squareSize, 
                                              square[1] / squareSize);
-
         board.printContext(context, squareSize, color);
-
-        board.highlight(context, squareSize, position[0] / squareSize, 
-                                             position[1] / squareSize, 
-                                             square[0] / squareSize, 
-                                             square[1] / squareSize);
-
-        //context.beginPath();
-        //context.rect(position[0], position[1], square[0], square[1]); 
-        //context.stroke();
+        //board.highlight(context, squareSize, position[0] / squareSize, 
+        //                                     position[1] / squareSize, 
+        //                                     square[0] / squareSize, 
+        //                                     square[1] / squareSize);
         setCount(count + 1);
     }, 50);
 
     const handleOnMouseDown = (e) => {
         let rect = ref.current.getBoundingClientRect();
-
-        console.log("Mouse down: " + (e.pageX - rect.left) + "," + (e.pageY - rect.top));
-
-
         let x = Math.round((e.pageX - rect.left) / 10) * 10,
             y = Math.round((e.pageY - rect.top) / 10) * 10;
-
-
         if (position[0] <= x && x <= position[0] + square[0] && 
             position[1] <= y && y <= position[1] + square[1]) {
             setDrag(true);
             setOffset([x - position[0], y - position[1]]);
-        }            
-
+        }
     } 
 
     const handleOnMouseUp = (e) => {
-        let rect = ref.current.getBoundingClientRect();
-        
         setDrag(false);
-
-        console.log("Mouse up: " + (e.pageX - rect.left) + "," + (e.pageY - rect.top));
     } 
 
     const handleOnMouseMove = (e) => {
         let rect = ref.current.getBoundingClientRect();
 
         if(drag){
-            console.log("Mouse move: " + (e.pageX - rect.left) + "," + (e.pageY - rect.top));
-
             let newX = Math.round((e.pageX - rect.left) / 10) * 10,
                 newY = Math.round((e.pageY - rect.top) / 10) * 10
-                
-
-
-
             setPosition([newX - offset[0], newY - offset[1]]);
         }
-        
     }
 
     const handleOnClick = (e) => {
         let rect = ref.current.getBoundingClientRect();
-        
+        let x = Math.floor(Math.round((e.pageX - rect.left) / 10)),
+            y = Math.floor(Math.round((e.pageY - rect.top) / 10));
 
-        console.log("Mouse click: " + (e.pageX - rect.left) + "," + (e.pageY - rect.top));
+        board.setXY(x, y, 1);
+
     }
     
     return (
