15a9485e4bf1f1b8206881f7f5c64ec6e3e24297
amaurs <amaury.gtz@gmail.com>
2019-11-28 15:10:52 -0800

    🌱 Trying to load the depth image.

--- src/Autostereogram.js
+++ src/Autostereogram.js
@@ -1,20 +1,19 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useRequestAnimationFrame } from './Hooks.js';
 import { intToColor } from './util.js';
 
 import Board from './Board.js';
 
-let board = new Board(500, 500);
+let board = new Board(100, 450);
 
 board.init();
 
 
-const Autostereogram = () => {
+const Autostereogram = (props) => {
 
     let ref = useRef();
     const squareSize = 1;
-
-
+    
     useRequestAnimationFrame(() => {
         board.randomize();
         let canvas = ref.current;
@@ -23,13 +22,17 @@ const Autostereogram = () => {
         context.clearRect(0, 0, canvas.width, canvas.height);
         board.printContext(context, squareSize, intToColor(0))
 
+        //context.putImageData(props.data, 0, 0);
+
     }, 100);
+
+    
 
     return (
             <canvas
-                ref={ref} 
-                width={500}
-                height={500}
+                ref={ref}
+                width={props.width}
+                height={props.height}
         />
         );
 }
