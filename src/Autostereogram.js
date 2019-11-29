b688a1741d6eafb723343594e7571942b45adec9
amaurs <amaury.gtz@gmail.com>
2019-11-29 11:15:02 -0800

    👨🏽‍👹 Autostereogram is working.

--- src/Autostereogram.js
+++ src/Autostereogram.js
@@ -8,23 +8,48 @@ let board = new Board(100, 450);
 
 board.init();
 
-
 const Autostereogram = (props) => {
-
     let ref = useRef();
     const squareSize = 1;
-    
+
     useRequestAnimationFrame(() => {
         board.randomize();
         let canvas = ref.current;
         let context = canvas.getContext('2d');
+        let frame  = new ImageData(
+                            new Uint8ClampedArray(props.frame.data),
+                            props.frame.width,
+                            props.frame.height
+                          );
+        for(let y = 0; y < props.height; y++) {
+            for(let x = 0; x < props.width; x++) {
+                let index = (y * props.width + x) * 4;
+                if (!(x < 100)) {
+                    let average = ((frame.data[index + 0] + 
+                                               frame.data[index + 1] + 
+                                               frame.data[index + 2]) / 3) / 255;
+                    let offset = Math.floor(average * (32 - 1));
+                    
+                    
+                    if (offset > 0) {
+                        let value = board.getXY((x % 100) + offset, y);
+                        board.setXY((x % 100), y, value);
+                    }   
+                }
+                if (board.getXY((x % 100), y)) {
+                    frame.data[index + 0] = 0; 
+                    frame.data[index + 1] = 0; 
+                    frame.data[index + 2] = 0;
+                } else {
+                    frame.data[index + 0] = 255; 
+                    frame.data[index + 1] = 255; 
+                    frame.data[index + 2] = 255;
+                }
 
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        board.printContext(context, squareSize, intToColor(0))
-
-        //context.putImageData(props.data, 0, 0);
-
-    }, 100);
+            }
+        }
+        context.putImageData(frame, 0, 0);
+    });
 
     
 
