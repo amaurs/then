312a8fc2b00415bc2cdf4e8685dd597b25fde534
amaurs <amaury.gtz@gmail.com>
2019-12-22 14:30:03 -0800

    📱 Added menu with react routing.

---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './NotFound.css'
+
+const NotFound = () => {
+    return (<div className="NotFound">
+              <p className="name">NotFound</p>
+            </div>);
+}
+
+export default NotFound;
