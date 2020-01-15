5fe1a1b20ffac6132b17566f24e3c735b6a5dbcb
amaurs <amaury.gtz@gmail.com>
2020-01-14 21:49:59 -0800

    ⏳ Loading of images.

--- src/Hilbert.js
+++ src/Hilbert.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 import './Hilbert.css';
 
-import hilbert from './assets/hilbert.png';
+import hilbert_cube from './assets/hilbert_cube_512_512.png';
+import hilbert_square from './assets/hilbert_square_512_512.png';
 
-const Hilbert = () => {
-    return (<img className="Hilbert" src={hilbert} />);
+const Hilbert = (props) => {
+
+    let mount = useRef();
+
+    useEffect(() => {
+        console.log("First call hilbert.")
+
+        const getData = (src) => {
+            return new Promise ((resolve, reject) => {
+                let img = new Image();
+                img.onload = (event) => {
+                    let image = event.target;
+                    let canvas = document.createElement('canvas');
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+                    let context = canvas.getContext('2d');
+                    context.drawImage(image, 0, 0);
+                    resolve(context.getImageData(0, 0, image.width, image.height))
+                }
+                img.onerror = reject
+                img.src = src
+            });
+        }
+
+        getData(hilbert_cube).then(imageData => {
+            console.log(imageData)
+        })
+
+        getData(hilbert_square).then(imageData => {
+            console.log(imageData)
+        })
+
+    }, []);
+
+
+    return (<canvas className="Hilbert" 
+                width={props.width + "px"} 
+                height={props.height + "px"} 
+                ref={mount} 
+            />);
 }
 
 export default Hilbert;
