41a53dec15beddb393a984642027be51e8598bb9
amaurs <amaury.gtz@gmail.com>
2020-02-11 09:24:48 -0800

    📽 Wigglegrams working with backend.

--- src/Wigglegram.js
+++ src/Wigglegram.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import video from './assets/emji.mp4'
 import './Wigglegram.css'
 
-const Wigglegram = () => {
-    return (<video autoPlay loop muted>
-                <source src={video} type="video/mp4"/>
-            </video>);
+import { getRandomInt } from './util.js';
+
+const Wigglegram = (props) => {
+
+    let image = useRef();
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+         // TODO: This function needs memoizing. Right now it is calling the service on every mount.
+         console.log("Fetching...");
+         fetch(props.url + "/wigglegrams", {
+
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(response => {
+            return response.json();
+          }).then(json => {
+            setData(json["images"]);
+          });
+
+    }, []);
+
+
+    useEffect(() => {
+
+        if (data !== null) {
+            let selected = data[getRandomInt(0, data.length)];
+            
+            image.current.src = selected.url;
+        }
+
+    }, [data]);
+
+    
+
+
+    return (<div>
+                <img ref={image} />
+            </div>);
 }
 
 export default Wigglegram;
