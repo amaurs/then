02096cad9a9d07c4d90828b69f352677ed8e8d3c
amaurs <amaury.gtz@gmail.com>
2020-03-23 00:04:43 -0700

    🔌 One endpoint to rule them all.

--- src/Nostalgia.js
+++ src/Nostalgia.js
@@ -2,15 +2,13 @@ import React, { useEffect, useState } from 'react';
 import './Nostalgia.css'
 import { useInterval } from './Hooks.js';
 
-const USER_SERVICE_URL = "https://azzhejgg0l.execute-api.us-east-1.amazonaws.com/api/boleros/en";
-
 export default function Nostalgia(props) {
     const [user, setUser] = useState("");
     const [count, setCount] = useState(0);
     const [delay, setDelay] = useState(null);
 
     const getPhrase = () => {
-        fetch(USER_SERVICE_URL)
+        fetch(props.url)
             .then(results => results.json())
             .then(data => {
                 setUser(data.sentence.split(" ").filter(word => "" !== word)
@@ -39,6 +37,6 @@ export default function Nostalgia(props) {
     }, delay);
 
     return (<div className="Nostalgia">
-                <h1>{user.slice(0, count)}</h1>
+                    <h1>{user.slice(0, count)}</h1>
            </div>);
 }
