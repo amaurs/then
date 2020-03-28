c0e0fabd3dd57803177fb2a04fca26d9a3982ed3
amaurs <amaury.gtz@gmail.com>
2020-03-28 12:13:13 -0700

    🧾 Removed colors from traveling salesman.

--- src/TravelingSalesman.js
+++ src/TravelingSalesman.js
@@ -10,38 +10,15 @@ const TravelingSalesman = (props) => {
 
     let mount = useRef();
     const [tick, setTick] = useState(0);
-    const [delay, setDelay] = useState(24);
-    const [data, setData] = useState({cities: [], hasFetched: true});
+    const [delay, setDelay] = useState(10);
     const [cities, setCities] = useState({cities: [], hasFetched: true});
-    const [color, setColor] = useState([0, 0, 0]);
     const [citiesToDraw, setCitiesToDraw] = useState([]);
     const squareSampling = 100;
-    const numberColors = 700;
-
-
-    
-
-    useEffect(() => {
-         // TODO: This function needs memoizing. Right now it is calling the service on every mount.
-        const colors = getRandomIntegerArray(numberColors * 3, 0, 256);
-        let colorsUrl = props.url + "/solve?cities=" + JSON.stringify(colors) + "&dimension=" + 3;
-        fetch(colorsUrl, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
-          .then(response => {
-            return response.json();
-          }).then(json => {
-            setData({cities: json, hasFetched: true});
-          });
-    }, [props.url]);
-
+    const numberColors = 500;
 
     useEffect(() => {
         // TODO: This function needs memoizing. Right now it is calling the service on every mount.
-        let cityPoints = getRandomIntegerArray(numberColors * 2, 0, squareSampling);
+        let cityPoints = getRandomIntegerArray(numberColors * 2, 1, squareSampling);
 
         let citiesUrl = props.url + "/solve?cities=" + JSON.stringify(cityPoints) + "&dimension=" + 2;
         fetch(citiesUrl, {
@@ -55,30 +32,25 @@ const TravelingSalesman = (props) => {
         }).then(json => {
           setCities({cities: json, hasFetched: true})
         });
-    }, [props]);
+    }, []);
 
     useEffect(() => {
         if (citiesToDraw.length > 0) {
-            
-
             const context = mount.current.getContext('2d');
             const width = mount.current.width;
             const height = mount.current.height;
             context.save();
             context.clearRect(0, 0, width, height);
-            context.fillStyle = colorToString(color[0], color[1], color[2]);
-            context.fillRect(0, 0, width, height);
             context.beginPath();
-            context.strokeStyle = invertColor(color[0], color[1], color[2]);
+            context.strokeStyle = invertColor(255, 255, 255);
             context.lineWidth = 5;
             for(let i=0; i < citiesToDraw.length; i+=2) {
                 context.lineTo(Math.floor(width * citiesToDraw[i] / squareSampling), Math.floor(height * citiesToDraw[i + 1] / squareSampling))    
             }
             context.stroke();
-
         }
 
-    }, [data, cities, citiesToDraw, color]);
+    }, [citiesToDraw]);
 
     useInterval(() => {
         const maxBound = (time, size) => {
@@ -89,7 +61,6 @@ const TravelingSalesman = (props) => {
                 return size;
             }
         }
-
         const minBound = (time, size) => {
             let t = time % (2 * size)
             if (t < size) {
@@ -98,24 +69,28 @@ const TravelingSalesman = (props) => {
                 return time % size;
             }
         }
-
-        let colorNumber = tick % (numberColors + 1);
-        let color = [data.cities[colorNumber * 3],
-                     data.cities[colorNumber * 3 + 1],
-                     data.cities[colorNumber * 3 + 2]];
-        setColor(color);
         let min = minBound(tick, numberColors + 1);
         let max = maxBound(tick, numberColors + 1);
         let citiesToDraw = cities.cities.slice(min * 2, max * 2);
         setCitiesToDraw(citiesToDraw);
         setTick(tick + 1);
-        setDelay(24);
     }, delay);
+
+    let style = {};
+    if (props.width > 0 && props.height > 0) {
+        style = props.width/props.height< 1 ? {width: "100%"}
+                                            : {height: "100%"};
+    }
+
+    let minSize = props.width/props.height < 1 ? props.width:
+                                                 props.height;
+    
+
     if (citiesToDraw.length > 0) {
         return (<canvas
                 ref={mount}
-                width={props.width + "px"}
-                height={props.height + "px"}
+                width={minSize}
+                height={minSize}
                 className="TravelingSalesman"
             />);
     } else {
