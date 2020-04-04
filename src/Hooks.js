c4b37951f55e7c5a692696c46e65995d269ce21e
amaurs <amaury.gtz@gmail.com>
2020-04-03 19:47:22 -0700

    ⏲ Adding timeout hook.

--- src/Hooks.js
+++ src/Hooks.js
@@ -42,3 +42,25 @@ export function useRequestAnimationFrame(animateCallback) {
     };
   }, [animateCallback]);
 }
+
+
+export function useTimeout(callback, delay) {
+    const savedCallback = useRef();
+    const timeoutRef = useRef();
+
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    useEffect(() => {
+
+        if (delay !== null) {
+            timeoutRef.current = setTimeout(() => {
+                savedCallback.current();
+            }, delay);
+
+            return () => clearTimeout(timeoutRef.current);
+        }
+    }, [delay]);
+
+}
