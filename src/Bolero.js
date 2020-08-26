3a79632415057b6d068409df3f0b4b90814796f3
amaurs <amaury.gtz@gmail.com>
2020-08-26 15:50:16 -0700

    📯 Using request animation frame in bolero.

--- src/Bolero.js
+++ src/Bolero.js
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './Bolero.css'
 import { useInterval, useTimeout } from './Hooks.js';
 import Loader from './Presentation.js';
 
 export default function Bolero(props) {
-    const [user, setUser] = useState("");
+
+    let mount = useRef();
+    const [sentence, setSentence] = useState("");
     const [count, setCount] = useState(0);
+    const [total, setTotal] = useState(0);
     const [delay, setDelay] = useState(null);
     const [presenting, setPresenting] = useState(true);
 
@@ -28,10 +31,11 @@ export default function Bolero(props) {
                 let response = await fetch(url, payload);
                 let json = await response.json();
                 if (!cancel) {
-                    setUser(json.sentence.split(" ").filter(word => "" !== word)
+                    setSentence(json.sentence.split(" ").filter(word => "" !== word)
                                                     .map(word => word === "i"? "I": word)
                                                     .map((word, index) => index === 0? word.charAt(0).toUpperCase() + word.slice(1): word)
                                                     .reduce((a, b) => a + " " + b, ""));
+                    setTotal(total + 1);
                 }  
             } catch (error) {
                 console.log("Call to order endpoint failed.", error)
@@ -39,21 +43,45 @@ export default function Bolero(props) {
         }
         getPhrase(props.url);
         return () => cancel=true;
-    }, [props.url]);
+    }, [props.url, count]);
 
-    useInterval(() => {
-        if(count < user.length) {
-            setCount(count + 1);
-            setDelay(100);
-        }
-    }, delay);
+
+    useEffect(() => {  
+        if (sentence.length > 0 && !presenting) {
+            let n = 0;
+            let timeoutId;
+
+            const animate = () => {
+                timeoutId = setTimeout(function() {
+                    console.log(sentence.slice(0, n));
+
+                    mount.current.innerHTML = sentence.slice(0, n);
+                    
+                    if(sentence.length === n - 5 && total < 10) {
+                        setCount(count + 1);
+                    }
+
+                    n += 1;
+                    frameId = requestAnimationFrame(animate);
+                }, 1000 / 10);
+             }
+
+            let frameId = requestAnimationFrame(animate);
+            return () => {
+                cancelAnimationFrame(frameId);
+                // It is important to clean up after the component unmounts.
+                clearTimeout(timeoutId);
+                frameId = null;
+            }
+        } 
+    }, [sentence, presenting]);
 
     if (presenting) {
         return <Loader title={props.title}/>
     } else {
         return (
             <div className="Bolero">
-                <h1>{user.slice(0, count)}</h1>
+                <h1 ref={mount}></h1>
             </div>
         );
     }
