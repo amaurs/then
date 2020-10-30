ee1a344a5d2200889c8f948dd54ff580adbf1731
amaurs <amaury.gtz@gmail.com>
2020-10-29 21:49:44 -0700

    🧵 Integrating loom project.

--- src/Board.js
+++ src/Board.js
@@ -4,10 +4,11 @@ const DEAD = 0;
 
 export default class Board {
 
-    constructor(width, height) {
+    constructor(width, height, seed) {
         this.height = height;
         this.width = width;
         this.board = [];
+        this.seed = seed || .5;
         this.init();
     }
 
@@ -170,5 +171,75 @@ export default class Board {
             }
         }
         return intToColor(color);
+    }
+
+    shiftDown() {
+        let result = new Board(this.width, this.height);
+        for(let j = this.height - 1; j > 0; j--) {
+            for(let i = 0; i < this.width; i++) {
+                result.setXY(i, j, this.getXY(i, j - 1));
+            }
+        }
+        for(let i = 0; i < this.width; i++) {
+            result.setXY(i, 0, Math.random() < this.seed? ALIVE: DEAD);
+        }
+        return result;
+    }
+
+    shiftUp() {
+        let result = new Board(this.width, this.height, this.seed);
+        for(let j = 0; j < this.height - 1; j++) {
+            for(let i = 0; i < this.width; i++) {
+                result.setXY(i, j, this.getXY(i, j + 1));
+            }
+        }
+        for(let i = 0; i < this.width; i++) {
+            result.setXY(i, this.height - 1, Math.random() < this.seed? ALIVE: DEAD);
+        }
+        return result;
+    }
+
+    shiftRight() {
+        let result = new Board(this.width, this.height, this.seed);
+        for(let j = 0; j < this.height; j++) {
+            for(let i = this.width - 1; i > 0; i--) {
+                result.setXY(i, j, this.getXY(i - 1, j));
+            }
+        }
+        for(let j = 0; j < this.height; j++) {
+            result.setXY(0, j, Math.random() < this.seed? ALIVE: DEAD);
+        }
+        return result;
+    }
+
+
+    multiply(other) {
+
+        console.assert(other.height === this.width, {errorMsg: "Board dimension do not match."});
+        let result = new Board(other.width, this.height);
+
+        for(let j = 0; j < this.height; j++) {
+            for(let i = 0; i < other.width; i++) {
+                let dot = 0;
+                for(let k = 0; k < other.height; k++) {
+                    dot += this.getXY(k, j) * other.getXY(i, k);
+                }
+                result.setXY(i, j, dot);
+            }
+        }
+ 
+        return result;
+    }
+
+    transpose() {
+        let result = new Board(this.height, this.width);
+
+        for(let j = 0; j < this.height; j++) {
+            for(let i = 0; i < this.width; i++) {
+                result.setXY(j, i, this.getXY(i,j));
+            }
+        }
+ 
+        return result;
     }
 }
