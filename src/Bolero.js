0b839a9d6ab7599dd9b4f43342da12de676bfd13
amaurs <amaury.gtz@gmail.com>
2020-11-16 23:55:43 -0800

    🎬  Making words bigger for Bolero.

--- src/Bolero.js
+++ src/Bolero.js
@@ -1,18 +1,17 @@
-import React, { useEffect, useState, useRef, useContext } from 'react';
-import { ThemeContext } from './ThemeContext.js';
-import './Bolero.css'
-import { useInterval, useTimeout } from './Hooks.js';
-import Loader from './Presentation.js';
+import React, { useEffect, useState, useRef, useContext } from "react";
+import { ThemeContext } from "./ThemeContext.js";
+import "./Bolero.css";
+import { useInterval, useTimeout } from "./Hooks.js";
+import Loader from "./Presentation.js";
 
 export default function Bolero(props) {
-
     let mount = useRef();
     const theme = useContext(ThemeContext);
     const [sentence, setSentence] = useState("");
     const [count, setCount] = useState(0);
     const [total, setTotal] = useState(0);
     const [delay, setDelay] = useState(null);
-    const [presenting, setPresenting] = useState(props.delay>0);
+    const [presenting, setPresenting] = useState(props.delay > 0);
 
     useTimeout(() => {
         setPresenting(false);
@@ -24,49 +23,57 @@ export default function Bolero(props) {
         const getPhrase = async (url) => {
             try {
                 let payload = {
-                  method: 'GET',
-                  headers: {
-                    'Content-Type': 'application/json'
-                  }
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
                 };
 
                 let response = await fetch(url, payload);
                 let json = await response.json();
                 if (!cancel) {
-                    setSentence(json.sentence.split(" ").filter(word => "" !== word)
-                                                    .map(word => word === "i"? "I": word)
-                                                    .map((word, index) => index === 0? word.charAt(0).toUpperCase() + word.slice(1): word)
-                                                    .reduce((a, b) => a + " " + b, ""));
+                    setSentence(
+                        json.sentence
+                            .split(" ")
+                            .filter((word) => "" !== word)
+                            .map((word) => (word === "i" ? "I" : word))
+                            .map((word, index) =>
+                                index === 0
+                                    ? word.charAt(0).toUpperCase() +
+                                      word.slice(1)
+                                    : word
+                            )
+                            .reduce((a, b) => a + " " + b, "")
+                    );
                     setTotal(total + 1);
-                }  
+                }
             } catch (error) {
-                console.log("Call to order endpoint failed.", error)
+                console.log("Call to order endpoint failed.", error);
             }
-        }
+        };
         getPhrase(props.url);
-        return () => cancel = true;
+        return () => (cancel = true);
     }, [props.url, count]);
 
-
-    useEffect(() => {  
+    useEffect(() => {
         if (sentence.length > 0 && !presenting) {
             let n = 0;
             let timeoutId;
 
             const animate = () => {
-                timeoutId = setTimeout(function() {
+                timeoutId = setTimeout(function () {
                     console.log(sentence.slice(0, n));
 
                     mount.current.innerHTML = sentence.slice(0, n);
-                    
-                    if(sentence.length === n - 5 && total < 10) {
+
+                    if (sentence.length === n - 5 && total < 10) {
                         setCount(count + 1);
                     }
 
                     n += 1;
                     frameId = requestAnimationFrame(animate);
-                }, 1000 / 10);
-             }
+                }, 1000 / 5);
+            };
 
             let frameId = requestAnimationFrame(animate);
             return () => {
@@ -74,19 +81,20 @@ export default function Bolero(props) {
                 // It is important to clean up after the component unmounts.
                 clearTimeout(timeoutId);
                 frameId = null;
-            }
-        } 
+            };
+        }
     }, [sentence, presenting]);
 
-    let style= {color: theme.theme.foreground, 
-                mixBlendMode: theme.theme.mixBlendMode};
+    let style = {
+        color: theme.theme.middleground,
+        mixBlendMode: theme.theme.mixBlendMode,
+    };
 
     if (presenting) {
-        return <Loader title={props.title}/>
+        return <Loader title={props.title} />;
     } else {
         return (
-            <div className="Bolero" 
-                 style={{ ...props.style, ...style }}>
+            <div className="Bolero" style={{ ...props.style, ...style }}>
                 <h1 ref={mount}></h1>
             </div>
         );
