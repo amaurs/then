960434eff0b39721fff4b45b3dddb806ff4ab7ee
amaurs <amaury.gtz@gmail.com>
2020-11-17 09:58:31 -0800

    🎮 Adding Konami theme.

--- src/Scaffold.js
+++ src/Scaffold.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { useLocalStorage } from './Hooks.js'; 
-import Home from './Something.js';
+import React from "react";
+import { useLocalStorage } from "./Hooks.js";
+import Home from "./Something.js";
 
-
-import { ThemeContext, themes   } from './ThemeContext.js'
-
+import { ThemeContext, themes } from "./ThemeContext.js";
 
 const Then = () => {
-
-    const [theme, setTheme] = useLocalStorage('theme', themes.light);
+    const [theme, setTheme] = useLocalStorage("theme", themes.light);
 
     const toggleTheme = () => {
-        setTheme(theme.name === "dark"?themes.light:themes.dark);
-    }
+        setTheme(theme.name === "dark" ? themes.light : themes.dark);
+    };
 
     return (
-        <ThemeContext.Provider value={{theme, toggleTheme}}>
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
             <Home />
-        </ThemeContext.Provider>);
-}
+        </ThemeContext.Provider>
+    );
+};
 
 export default Then;
--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,30 +1,38 @@
-import React from 'react';
+import React from "react";
 
 const themes = {
-  light: {
-    foreground: "magenta",
-    background: "#ffffff",
-    middleground: "#161011",
-    slider: false,
-    name: "light",
-    mixBlendMode: "multiply",
-    mixBlendModeBackground: "difference"
-  },
-  dark: {
-    foreground: "#f0a5a3",
-    background: "#161011",
-    middleground: "#ffffff",
-    slider: true,
-    name: "dark",
-    mixBlendMode: "difference",
-    mixBlendModeForeground: "multiply"
-  }
+    light: {
+        foreground: "magenta",
+        background: "#ffffff",
+        middleground: "#161011",
+        slider: false,
+        name: "light",
+        mixBlendMode: "multiply",
+        mixBlendModeBackground: "difference",
+    },
+    dark: {
+        foreground: "#f0a5a3",
+        background: "#161011",
+        middleground: "#ffffff",
+        slider: true,
+        name: "dark",
+        mixBlendMode: "difference",
+        mixBlendModeForeground: "multiply",
+    },
+    konami: {
+        foreground: "red",
+        background: "blue",
+        middleground: "green",
+        slider: true,
+        name: "konami",
+        mixBlendMode: "normal",
+        mixBlendModeForeground: "screen",
+    },
 };
 
-
-
-const ThemeContext = React.createContext({theme: themes.light,
-                                          toggleTheme: () => {} });
-
+const ThemeContext = React.createContext({
+    theme: themes.light,
+    toggleTheme: () => {},
+});
 
 export { ThemeContext, themes };
