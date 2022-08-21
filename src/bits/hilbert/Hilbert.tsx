edb60105fc1c96cf3cfa9ce7cd9298948342be10
amaurs <amaury.gtz@gmail.com>
2022-08-21 13:32:30 -0700

    🌈 Using composition to reuse code for all color bits.

--- src/bits/hilbert/Hilbert.tsx
+++ src/bits/hilbert/Hilbert.tsx
@@ -12,6 +12,7 @@ import hilbert_cube_512 from "../../assets/hilbert_cube_512_512.png";
 import hilbert_square_512 from "../../assets/hilbert_square_512_512.png";
 import hilbert_cube_4096 from "../../assets/hilbert_cube_4096_4096.png";
 import hilbert_square_4096 from "../../assets/hilbert_square_4096_4096.png";
+import Animation from "../../util/Animation";
 
 import { ThemeContext } from "../../ThemeContext.js";
 
@@ -45,152 +46,32 @@ const Hilbert = (props: Props) => {
         res = "512";
     }
 
-    let canvas = useRef<HTMLCanvasElement>(document.createElement("canvas"));
-    let theme = useContext(ThemeContext);
-    let [color, setColor] = useState<ImageData | undefined>(undefined);
-    let [position, setPosition] = useState<ImageData | undefined>(undefined);
-    const [presenting, setPresenting] = useState(props.delay > 0);
+    let [cube, setCube] = useState<string | undefined>(image_cube_map.get(res!));
+    let [square, setSquare] = useState<string | undefined>(image_square_map.get(res!));
 
-    useTimeout(() => {
-        setPresenting(false);
-    }, props.delay);
 
     useEffect(() => {
         let hilbert_cube = image_cube_map.get(res!);
         let hilbert_square = image_square_map.get(res!);
-        let cancel = false;
-
-        const getData = (src: string): Promise<ImageData> => {
-            return new Promise((resolve, reject) => {
-                let img = new Image();
-                img.onload = (event: Event) => {
-                    let canvas: HTMLCanvasElement = document.createElement(
-                        "canvas"
-                    );
-                    let image = event.target as HTMLImageElement;
-                    canvas.width = image.width;
-                    canvas.height = image.height;
-                    let context: CanvasRenderingContext2D = canvas.getContext(
-                        "2d"
-                    )!;
-                    context.drawImage(image, 0, 0);
-                    resolve(
-                        context.getImageData(0, 0, image.width, image.height)
-                    );
-                };
-                img.onerror = reject;
-                img.src = src;
-            });
-        };
-
-        getData(hilbert_cube!).then((imageData: ImageData) => {
-            if (!cancel) {
-                setColor(imageData);
-            }
-        });
-
-        getData(hilbert_square!).then((imageData: ImageData) => {
-            if (!cancel) {
-                setPosition(imageData);
-            }
-        });
-
-        return () => {
-            cancel = true;
-        };
+        setCube(hilbert_cube);
+        setSquare(hilbert_square);
+        return () => {};
     }, [res]);
 
-    useEffect(() => {
-        if (color !== undefined && position !== undefined && !presenting) {
-            let count = 0;
-            let timeoutId: any;
 
-            const animate = () => {
-                // Wrapping the animation function wiht a timeout makes it
-                // possible to control the fps, without losing the benefits of
-                // requestAnimationFrame.
-                timeoutId = setTimeout(function () {
-                    const context: CanvasRenderingContext2D = canvas.current.getContext(
-                        "2d"
-                    )!;
-                    context.imageSmoothingEnabled = false;
-                    let canvasWidth = canvas.current.width;
-                    let canvasHeight = canvas.current.height;
-                    context.clearRect(0, 0, canvasWidth, canvasHeight);
-                    let frame = context.getImageData(
-                        0,
-                        0,
-                        canvasWidth,
-                        canvasHeight
-                    );
-
-                    let l = frame.data.length / 4;
-
-                    for (let i = 0; i < l; i++) {
-                        let index = (i + count) % l;
-
-                        let r = position!.data[index * 4 + 0];
-                        let g = position!.data[index * 4 + 1];
-                        let b = position!.data[index * 4 + 2];
-                        let j = colorToInt(r, g, b);
-
-                        if (theme.theme.name == 'konami') {
-                            frame.data[j * 4 + 0] = 255;
-                            frame.data[j * 4 + 1] = i * 4;
-                            frame.data[j * 4 + 2] = 255;
-                            frame.data[j * 4 + 3] = 255;
-                        } else if (theme.theme.name == 'light') {
-                            frame.data[j * 4 + 0] = 255 - color!.data[i * 4 + 0];
-                            frame.data[j * 4 + 1] = 255 - color!.data[i * 4 + 1];
-                            frame.data[j * 4 + 2] = 255 - color!.data[i * 4 + 2];
-                            frame.data[j * 4 + 3] = 255;
-                        } else {
-                            frame.data[j * 4 + 0] = color!.data[i * 4 + 0];
-                            frame.data[j * 4 + 1] = color!.data[i * 4 + 1];
-                            frame.data[j * 4 + 2] = color!.data[i * 4 + 2];
-                            frame.data[j * 4 + 3] = 255;
-                        }
-                        
-                        
-                    }
-                    context.putImageData(frame, 0, 0);
-
-                    frameId = requestAnimationFrame(animate);
-                    count += 256;
-                }, 1000 / 250);
-            };
-
-            let frameId: number | null = requestAnimationFrame(animate);
-            return () => {
-                cancelAnimationFrame(frameId!);
-                // It is important to clean up after the component unmounts.
-                clearTimeout(timeoutId);
-                frameId = null;
-            };
-        }
-    }, [color, position, presenting, theme]);
-
-    let style = {};
-    if (props.width > 0 && props.height > 0) {
-        style =
-            props.width / props.height < 1
-                ? { width: "100vw" }
-                : { height: "100vh" };
-    }
-
-    if (presenting) {
-        return <Loader title={props.title} />;
-    } else {
-        return (
-            <canvas
-                className="Hilbert"
-                style={{ ...props.style, ...style }}
-                width={res + "px"}
-                height={res + "px"}
-                ref={canvas}
-            />
-        );
-    }
+    return (
+        <Animation
+            className="Hilbert"
+            title={props.title}
+            width={props.width}
+            height={props.height}
+            delay={props.delay}
+            style={props.style}
+            res={res}
+            square={square}
+            cube={cube}
+        />
+    );
 };
 
 export default Hilbert;
